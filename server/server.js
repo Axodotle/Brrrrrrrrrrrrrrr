907b0d7651f5f713a70d4e9a24a54e9c3bcd957d
artmontinski <artmontinski@gmail.com>
2024-06-15 11:49:15 -0700

    a couple minor things

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/cuisine', spoonacularController.getByCuisine, (req, res) => {
+  res.status(200).end('yumyums');
+})
+
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  let defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred - GEH' },
+  };
+
+  const errorObj = Object.assign(defaultErr, err);
+  res.status(errorObj.status).json(errorObj.message); //either it returns the default or the updated err
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
