2b9a08d5f82df934ff467766da1adbff5434575b
James Coen <jcoen96@gmail.com>
2024-06-15 11:25:09 -0600

    add npm scripts for front and backend servers

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import App from './components/App.jsx';
+import './styling/styles.css'; // we will have to check this later
 
 //creating root for JSX
 const root = createRoot(document.getElementById('root'));
 //rendering root / app
-createRoot.render(
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
